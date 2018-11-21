91c9ae78f1c9e8741e9deaa652f11ef171769cea
Adam <adam@iii.com>
2018-11-21 18:49:12 +0800

    test word width

---
+++ hello-java-app/src/test/java/fullhalfwidth/TestFullHalfWidth.java
@@ -0,0 +1,13 @@
+package fullhalfwidth;
+
+import org.junit.Test;
+
+public class TestFullHalfWidth {
+	
+	@Test
+	public void test() {
+		System.out.println("AAA");
+		System.out.println("ＡＡＡ");
+	}
+
+}
