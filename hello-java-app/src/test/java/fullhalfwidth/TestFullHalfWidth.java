3488f30ba6271a6a642e6416df1a49f02e36fbde
Adam <adam@iii.com>
2018-11-21 19:10:04 +0800

    test fullwidth halfwidth

--- hello-java-app/src/test/java/fullhalfwidth/TestFullHalfWidth.java
+++ hello-java-app/src/test/java/fullhalfwidth/TestFullHalfWidth.java
@@ -7,7 +7,18 @@ public class TestFullHalfWidth {
 	@Test
 	public void test() {
 		System.out.println("AAA");
+		System.out.println("\u0041");
+		System.out.println("A".equals("\u0041"));
+		System.out.println();
 		System.out.println("ＡＡＡ");
+		System.out.println("\uff21");
+		System.out.println("Ａ".equals("\uff21"));
+		System.out.println();
+		System.out.println("，");
+		System.out.println(",");
+		System.out.println( "\\u" + Integer.toHexString('，' | 0x10000).substring(1) );
+		System.out.println(Integer.toHexString('，' | 0x0000));
+		System.out.println("，".equals("\uff0c"));
 	}
 
 }
