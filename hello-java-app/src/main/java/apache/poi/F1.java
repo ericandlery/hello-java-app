17941599ff37f16f8eaff9882006813b40c3230d
Adam <adam@iii.com>
2018-12-07 00:38:59 +0800

    apache poi excel sample

---
+++ hello-java-app/src/main/java/apache/poi/F1.java
@@ -0,0 +1,34 @@
+package apache.poi;
+
+public class F1 {
+	
+	private String team;
+	private String driver1;
+	private String driver2;
+	
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public String getDriver1() {
+		return driver1;
+	}
+	public void setDriver1(String driver1) {
+		this.driver1 = driver1;
+	}
+	public String getDriver2() {
+		return driver2;
+	}
+	public void setDriver2(String driver2) {
+		this.driver2 = driver2;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
---
+++ hello-java-app/src/main/java/apache/poi/F1Reader.java
@@ -0,0 +1,68 @@
+package apache.poi;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class F1Reader {
+
+	public static void main(String[] args) throws java.lang.Exception {
+		
+		System.out.println("POI GO");
+		
+		String fileName="E:\\Java Test IO\\POI\\excel\\f1.xlsx";
+		List<F1> f1List=new ArrayList<>();
+		
+		FileInputStream fis=null;
+		Workbook workbook=null;
+		
+		try {
+			
+			fis=new FileInputStream(fileName);
+			
+			if(fileName.toLowerCase().endsWith("xlsx")) {
+				workbook=new XSSFWorkbook(fis);
+			}else if(fileName.toLowerCase().endsWith("xls")) {
+				workbook=new HSSFWorkbook(fis);
+			}else {
+				throw new RuntimeException();
+			}
+			
+			int num=workbook.getNumberOfSheets();
+			System.out.println("number of sheets=>"+num);
+			
+			for(int i=0;i<num;i++) {
+				Sheet sheet=workbook.getSheetAt(i);
+				Iterator<Row> rows=sheet.iterator();
+				
+				while(rows.hasNext()) {
+					Row row=rows.next();
+					Iterator<Cell> cells=row.iterator();
+					
+					while(cells.hasNext()) {
+						Cell cell=cells.next();
+						System.out.println(cell.getAddress()+"++"+cell.getStringCellValue());
+					}
+				}
+			}
+			
+		}catch(Exception e) {
+			throw new Exception();
+		}finally {
+			fis.close();
+			workbook.close();
+		}
+		
+		System.out.println("POI END");
+
+	}
+
+}
